cbf09fada6407db29bcaa872eb55c03a72c88836
Alexis Sanchez <psialedev@gmail.com>
2021-02-26 10:47:26 -0600

    Added auth constants, actions and reducers files to Redux folder

--- src/containers/Auth.js
+++ src/containers/Auth.js
@@ -5,6 +5,8 @@ import SignupForm from '../components/SignupForm';
 
 const Auth = () => {
   const history = useHistory();
+  //I can pass a string parameter for the reducer to handle the function
+  // login and signup
   const handleSubmit = () => {
     // do this with the store, I think maybe a isAuth boolean to make it work
     history.push('/dashboard');
