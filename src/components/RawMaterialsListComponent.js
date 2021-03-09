1d9d0e9e020627e0c43f38784d95961de21f7071
Alexis Sanchez <psialedev@gmail.com>
2021-03-09 14:09:43 -0600

    adding react bootstrap modal

--- src/components/RawMaterialsListComponent.js
+++ src/components/RawMaterialsListComponent.js
@@ -37,7 +37,7 @@ const RawMaterialsListComponent = ({
       <Multiselect
         options={rawMaterials}
         displayValue="name"
-        closeOnSelect="false"
+        closeOnSelect={false}
         ref={selectedMaterials}
         onSelect={handleSelect}
       />
