5507a0d544f78f5aac085978e3e92e4228a198d6
Alexis Sanchez <psialedev@gmail.com>
2021-03-12 15:11:16 -0600

    Added fetchGeTRawMaterials to render the global circular progress

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -9,12 +9,12 @@ import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 import buildLoader from '../components/Loader';
-import { fetchBusinessGetData } from '../redux/actions/data';
+import { fetchBusinessGetData, fetchGetRawMaterials } from '../redux/actions/data';
 import { retrieveItem } from '../helpers';
 import BusinessComponent from '../components/setters/BusinessComponent';
 
 const Home = ({
-  loading, isAuth, business, fetchBusinessGetData,
+  loading, isAuth, business, fetchBusinessGetData, rawMaterials, fetchGetRawMaterials
 }) => {
   if (loading === true && isAuth === false) {
     return buildLoader();
@@ -26,6 +26,9 @@ const Home = ({
     if (authToken === '') history.goBack();
     axios.defaults.headers.common = { Authorization: `Bearer ${authToken}` };
     fetchBusinessGetData('business');
+    if (rawMaterials !== [] && business !== null) {
+      fetchGetRawMaterials(`business/${business.id}/raw_materials`);
+    }
   }, []);
 
   const handleOnClick = endpoint => history.push(endpoint);
@@ -50,13 +53,16 @@ const mapStateToProps = state => ({
   loading: state.authStore.loading,
   isAuth: state.authStore.is_auth,
   business: state.dataStore.business,
+  rawMaterials: state.dataStore.raw_materials,
 });
 
 const mapDispatchToProps = dispatch => ({
   fetchBusinessGetData: endpoint => dispatch(fetchBusinessGetData(endpoint)),
+  fetchGetRawMaterials: endpoint => dispatch(fetchGetRawMaterials(endpoint)),
 });
 
 Home.propTypes = {
+  fetchGetRawMaterials: Proptypes.func.isRequired,
   loading: Proptypes.bool.isRequired,
   isAuth: Proptypes.bool.isRequired,
   business: Proptypes.shape({
@@ -66,6 +72,15 @@ Home.propTypes = {
     owner_id: Proptypes.number,
   }).isRequired,
   fetchBusinessGetData: Proptypes.func.isRequired,
+  rawMaterials: Proptypes.arrayOf(Proptypes.shape({
+    name: Proptypes.string,
+    total_amount: Proptypes.number,
+    remaining_amount: Proptypes.number,
+  })),
 };
+
+Home.defaultProps = {
+  rawMaterials: [],
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
