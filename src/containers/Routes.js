88a1a03ab6237d8f7bcd5e331c2cb1e2e9f2affb
Alexis Sanchez <psialedev@gmail.com>
2021-03-16 22:21:47 -0600

    Added root styling

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -2,15 +2,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import RawMaterialsListComponent from './RawMaterialsListComponent';
 import Auth from './Auth';
 import Home from './Home';
+import styles from './Routes.module.css';
 
 const Routes = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Auth} />
-      <Route path="/dashboard" component={Home} />
-      <Route path="/business/raw_materials" component={RawMaterialsListComponent} />
-    </Switch>
-  </BrowserRouter>
+  <div className={styles.rootContainer}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Auth} />
+        <Route path="/dashboard" component={Home} />
+        <Route path="/business/raw_materials" component={RawMaterialsListComponent} />
+      </Switch>
+    </BrowserRouter>
+  </div>
 );
 
 export default Routes;
