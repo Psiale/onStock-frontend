c34fd77c8bb0ac307b30b2e320f124441ffd6333
Alexis Sanchez <psialedev@gmail.com>
2021-03-25 17:37:21 -0600

    Changed timeout of loader so the owner won't think the loader is running infinitely

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Proptypes from 'prop-types';
-import styles from './Modal.module.css';
 
 const ModalComponent = ({
   title,
